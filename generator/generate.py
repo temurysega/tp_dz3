fc70793c08f44c8d4185a3805b84cf1af614accd
temurysega <txurmatullin@inbox.ru>
2026-06-12 12:23:16 +0300

    поправлены столбцы

--- generator/generate.py
+++ generator/generate.py
@@ -5,18 +5,15 @@ import sys
 
 NUM_ROWS = 50
 
-
-COLUMNS = ["COLUMN_1", "COLUMN_2", "COLUMN_3", "COLUMN_4"]
+COLUMNS = ["id", "category", "left","price"]
 
 def generate_row():
-
     return {
-        "COLUMN_1": random.randint(0, 100),
-        "COLUMN_2": round(random.uniform(1.5, 9.9), 2),
-        "COLUMN_3": random.randint(0, 100),
-        "COLUMN_4": random.choice(["A", "B", "C"]),
+        "id": random.randint(0, 100),
+        "price": round(random.uniform(1.5, 9.9), 2),
+        "left": random.randint(0, 100),
+        "category": random.choice(["A", "B", "C"]),
     }
-
 OUTPUT_DIR = sys.argv[1] if len(sys.argv) > 1 else "/data"
 OUTPUT_FILE = os.path.join(OUTPUT_DIR, "data.csv")
 
